195792e85330c1a1ba3f5bab06fb30d620f2ebcd
Alba de León Hernández <alu0100831016@ull.edu.es>
2014-03-19 14:00:32 +0000

    falta la tabla

--- src/uso_modulo.py
+++ src/uso_modulo.py
@@ -1,8 +1,18 @@
 #! /usr/bin/python
+
+def imprim(l):
+  print "i \t t_upla0 \t t_upla1 \t t_upla2 \t t_upla3\t t_upla4 \t t_upla5 \t"
+  o=0
+  for 
 import modulo
-t_upla= (10, 100, 1000, 10000, 100000, 1000000)
+
+t_upla= (10, 100, 1000, 10000, 100000, 1000000) 
 l = [ ]
-for i in range(len(t_upla)):
-  a= modulo.aprxpi(t_upla[i])
+for i in t_upla:
+  a= modulo.aprxpi(i)
   l=l+[a]
 print l
+
+
+# La t_upla tiene seis elementos.
+# La extension .pyc es la version compilada de un modulo ya interpretado.
